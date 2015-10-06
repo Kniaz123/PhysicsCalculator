eaef375f2684eef6f05e9b27c685b4b82709f3a5
Kniaz123 <zdurovandrei@mail.ru>
2015-10-06 15:05:59 +0300

    added different values

---
+++ PhysicsCalculator/Values.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsCalculator
+{
+    class Values
+    {
+        public enum TypeValue
+        {
+            Speed,
+            Time,
+            Distance
+        }
+
+        public Dictionary<string, string> DictionaryValue = new Dictionary<string, string>();
+    }
+}
